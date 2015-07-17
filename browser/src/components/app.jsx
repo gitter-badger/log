695d1fb3fe9678a0389f041da160fe22ba3287fe
Evan Mark Hopkins <evanmarkhopkins@gmail.com>
2015-07-17 15:09:54 -0500

    adds dummy data

--- browser/src/components/app.jsx
+++ browser/src/components/app.jsx
@@ -5,10 +5,28 @@ import Feed from './feed.jsx'
 import Nav from './nav.jsx'
 
 let App = React.createClass({
+  getInitialState () {
+    /* dummy data */
+    return ({
+      feed: [
+	{ date: 'Dec 16th 2005', body: 'Make breakfast' },
+	{ date: 'Oct 12th 2011', body: 'Sleep on the couch' },
+	{ date: 'March 1st 2014', body: 'fix the toaster' },
+	{ date: 'March 3rd 2014', body: 'eat all of the shoes' },
+	{ date: 'March 14th 2014', body: 'watch a bug\'s life' },
+	{ date: 'June 2nd 2015', body: 'Swim to the bahamalamas' },
+	{ date: 'March 18th 2016', body: 'fix the toaster (again)' },
+	{ date: 'Dec 20th 3030', body: 'build a time machine' },
+	{ date: 'June 2nd 2015', body: 'Swim to the bahamalamas' },
+	{ date: 'March 18th 2016', body: 'stop breaking the toaster' },
+	{ date: 'June 2nd 2015', body: 'Swim to the bahamalamas (again)' },
+	{ date: 'March 18th 2016', body: 'stalk Brad Pitt' }]
+    })
+  },
   render () {
     return (
 	<body>
-	  <Feed />
+	  <Feed data={this.state.feed} />
 	  <Nav />
 	</body>
     )
--- browser/src/components/feed.jsx
+++ browser/src/components/feed.jsx
@@ -4,6 +4,20 @@ import React from 'react'
 
 export default React.createClass({
   render () {
-    return <main/>
+
+    var todos = this.props.data.map(function (todo) {
+      return (
+	  <article>
+	    <date>{todo.date}</date>
+	    <p>{todo.body}</p>
+	  </article>
+      )
+    })
+
+    return (
+    	<main>
+	  {todos}
+        </main>
+    )
   }
 })
