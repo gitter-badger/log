e1a45dfaf61de9738aaf6eedbad49b22866ebe9e
Evan Mark Hopkins <evanmarkhopkins@gmail.com>
2015-07-18 11:49:59 -0500

    adjusts nav for mobile

--- browser/components/nav.jsx
+++ browser/components/nav.jsx
@@ -19,15 +19,15 @@ export default React.createClass({
 
     return (
 	<nav>
-	<svg className={plusClass} onClick={this.props.toggle} version="1.1" width="64" height="64">
-	    <g fill="#8A8A8A" transform="translate(16, 16)">
+	<svg className={plusClass} onClick={this.props.toggle} version="1.1" width="96" height="96">
+	    <g fill="#8A8A8A" transform="translate(32, 32)" >
 	      <rect y="12" rx="2" ry="2" width="32" height="8" />
 	      <rect x="12" rx="2" ry="2" width="8" height="32" />
 	    </g>
         </svg>
 	<svg className={checkClass} onClick={this.props.toggle} version="1.1"
-             width="64" height="64" style={{visibility: this.props.editing ? 'visible' : 'hidden'}}>
-	  <g fill="green" transform="translate(16, 16)">
+             width="96" height="96" >
+	  <g fill="#1ABC9C" transform="translate(32, 32)">
 	    <rect x="12" rx="2" ry="2" width="8" height="32" transform="rotate(45 16 16)" />
 	    <rect y="13" rx="2" ry="2" width="8" height="16" transform="rotate(135 4 21)" />
 	  </g>
